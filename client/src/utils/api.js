1c2e9427db94cccff6f458e06cf0b424335b008a
Canberk Varli <canberkvarli@gmail.com>
2025-01-18 09:37:46 -0800

    add console log for debugging

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+console.log("📢 VITE_API_URL:", import.meta.env.VITE_API_URL); // Debugging
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
 const api = axios.create({
   baseURL: API_URL,
   headers: {
