0a04088807ea854d0d45cb24dceae30abe205f5b
Canberk Varli <canberkvarli@gmail.com>
2025-01-18 07:36:42 -0800

    configured for render deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5001", // Replace with your backend server URL
+        target: API_URL,
         changeOrigin: true,
         secure: false,
       },
