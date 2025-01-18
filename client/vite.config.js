1317d006f6c46e6ae948add71f51286c0ceed0e4
Canberk Varli <canberkvarli@gmail.com>
2025-01-18 07:51:32 -0800

    configure vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+export default defineConfig(({ mode }) => {
+  // Load environment variables correctly without using process
+  const env = loadEnv(mode, import.meta.url, "VITE_");
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: API_URL,
-        changeOrigin: true,
-        secure: false,
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:5001",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
